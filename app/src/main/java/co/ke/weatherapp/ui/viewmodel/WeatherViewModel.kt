ec865bba605f545cbc41717fdcf400d1e8887268
Kenneth Mathari <mathari580@gmail.com>
2024-02-23 17:07:09 +0300

    Update WeatherViewModel.kt

--- app/src/main/java/co/ke/weatherapp/ui/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/co/ke/weatherapp/ui/viewmodel/WeatherViewModel.kt
@@ -3,9 +3,11 @@ package co.ke.weatherapp.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.ke.weatherapp.data.network.utils.NetworkResult
-import co.ke.weatherapp.data.repository.WeatherRepositoryImpl
+import co.ke.weatherapp.domain.location.LocationTracker
+import co.ke.weatherapp.domain.repository.WeatherRepository
 import co.ke.weatherapp.ui.state.WeatherState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,7 +19,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class WeatherViewModel @Inject constructor(
-    private val currentWeatherRepository: WeatherRepositoryImpl
+    private val weatherRepository: WeatherRepository,
+    private val locationTracker: LocationTracker,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ViewModel() {
 
     private val _weatherState = MutableStateFlow(WeatherState())
@@ -26,45 +30,58 @@ class WeatherViewModel @Inject constructor(
     val apiKey = System.getProperty("api.key")
 
 
-    //    init {
-//        getCurrentWeather()
-//    }
-    private fun getCurrentWeather(latitude: Double, longitude: Double) {
-        viewModelScope.launch(Dispatchers.IO) {
+    private fun getCurrentWeather() {
+        viewModelScope.launch(ioDispatcher) {
 
-            currentWeatherRepository.getCurrentWeather(
-                latitude,
-                longitude,
-                apiKey
-            ).buffer()
-                .collect { result ->
-                    when (result) {
-                        is NetworkResult.Success -> {
-                            _weatherState.update { currentWeatherState ->
-                                currentWeatherState.copy(
-                                    currentWeather = result.data
-                                )
+            _weatherState.update { currentWeatherState ->
+                currentWeatherState.copy(
+                    isLoading = true,
+                    errorMessage = null
+                )
+            }
+
+            locationTracker.getCurrentLocation()?.let { location ->
+                weatherRepository.getCurrentWeather(
+                    latitude = location.latitude,
+                    longitude = location.longitude,
+                    apiKey = apiKey
+                ).buffer()
+                    .collect { result ->
+                        when (result) {
+                            is NetworkResult.Success -> {
+                                _weatherState.update { currentWeatherState ->
+                                    currentWeatherState.copy(
+                                        currentWeather = result.data,
+                                        isLoading = false,
+                                        errorMessage = null
+                                    )
+                                }
                             }
+
+                            is NetworkResult.Error -> {
+                                _weatherState.update { currentWeatherState ->
+                                    currentWeatherState.copy(
+                                        currentWeather = null,
+                                        isLoading = false,
+                                        errorMessage = result.errorDetails.message
+                                    )
+                                }
+                            }
+
+                            else -> {}
                         }
 
-                        is NetworkResult.Loading -> {
-                            _weatherState.update { currentWeatherState ->
-                                currentWeatherState.copy(
-                                    isLoading = true
-                                )
-                            }
-                        }
-
-                        is NetworkResult.Error -> {
-                            _weatherState.update { currentWeatherState ->
-                                currentWeatherState.copy(
-                                    errorMessage = result.errorDetails.message
-                                )
-                            }
-                        }
                     }
 
+            } ?: kotlin.run {
+                _weatherState.update { currentWeatherState ->
+                    currentWeatherState.copy(
+                        isLoading = false,
+                        errorMessage = "Couldn't retrieve location. Make sure to grant " +
+                                "permission and enable GPS"
+                    )
                 }
+            }
 
         }
     }
