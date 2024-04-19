bdd6cde443dd25bbd68be3fda6b5a7a16b16725d
Kenneth Mathari <mathari580@gmail.com>
2024-04-19 12:56:22 +0300

    Update LoadingScreen.kt

--- app/src/main/java/co/ke/weatherapp/ui/screens/LoadingScreen.kt
+++ app/src/main/java/co/ke/weatherapp/ui/screens/LoadingScreen.kt
@@ -3,13 +3,19 @@ package co.ke.weatherapp.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import co.ke.weatherapp.ui.theme.WeatherAppTheme
 
 @Composable
 fun LoadingScreen(
@@ -20,6 +26,19 @@ fun LoadingScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        CircularProgressIndicator()
+        CircularProgressIndicator(
+            modifier = modifier.width(70.dp).height(70.dp).padding(10.dp),
+            color = Color.Blue
+        )
+        Text(text = "Fetching Weather Information...")
     }
+}
+
+@Preview
+@Composable
+fun PreviewLoadingScreen(){
+    WeatherAppTheme {
+        LoadingScreen(modifier = Modifier)
+    }
+
 }
