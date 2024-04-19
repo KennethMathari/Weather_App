502d26e904f825a1dbf3ca1ff6f7e5be6be1d4cc
Kenneth Mathari <mathari580@gmail.com>
2024-04-19 15:29:39 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,22 +25,11 @@ android {
     }
 
     buildTypes {
-        getByName("release") {
-            isShrinkResources = true
-            isMinifyEnabled = true
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android.txt"),
-//                "proguard-rules.pro"
-//            )
-        }
-
-        getByName("debug") {
-            isShrinkResources = true
-            isMinifyEnabled = true
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android.txt"),
-//                "proguard-rules.pro"
-//            )
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
